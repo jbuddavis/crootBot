3c97e1264350ab6f805734b56efeb3ca48c1ccb3
Bud Davis <75027599+jbuddavis@users.noreply.github.com>
2022-11-03 12:25:28 -0400

    Update and rename opp_adj_script.py to post_tweet.py

---
+++ post_tweet.py
@@ -0,0 +1,39 @@
+import os
+import tweepy
+
+try:
+    twitter_ck = os.environ["TWITTER_CK"]
+    twitter_cs = os.environ["TWITTER_CS"]
+    twitter_t = os.environ["TWITTER_T"]
+    twitter_ts = os.environ["TWITTER_TS"]
+except KeyError:
+    twitter_ck = "key not available!"
+    twitter_cs = "key not available!"
+    twitter_t = "key not available!"
+    twitter_ts = "key not available!"
+    
+print("TEST_SUCCESS")
+    
+def main():
+    twitter_auth_keys = {
+        "consumer_key"        : twitter_ck,
+        "consumer_secret"     : twitter_cs,
+        "access_token"        : twitter_t,
+        "access_token_secret" : twitter_ts
+    }
+ 
+    auth = tweepy.OAuthHandler(
+            twitter_auth_keys['consumer_key'],
+            twitter_auth_keys['consumer_secret']
+            )
+    auth.set_access_token(
+            twitter_auth_keys['access_token'],
+            twitter_auth_keys['access_token_secret']
+            )
+    api = tweepy.API(auth)
+ 
+    tweet = "HELLO WORLD"
+    status = api.update_status(status=tweet)
+ 
+if __name__ == "__main__":
+    main()
