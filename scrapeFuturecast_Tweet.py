d0a18e5bcec021b7c9eed44a35b6bd295cf92d71
Bud Davis <75027599+jbuddavis@users.noreply.github.com>
2022-11-16 14:52:56 -0500

    Add files via upload

--- scrapeFuturecast_Tweet.py
+++ scrapeFuturecast_Tweet.py
@@ -1,19 +1,19 @@
-# import os
+import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-# import tweepy
+import tweepy
 
-# try:
-#     twitter_ck = os.environ["TWITTER_CK"]
-#     twitter_cs = os.environ["TWITTER_CS"]
-#     twitter_t = os.environ["TWITTER_T"]
-#     twitter_ts = os.environ["TWITTER_TS"]
-# except KeyError:
-#     twitter_ck = "key not available!"
-#     twitter_cs = "key not available!"
-#     twitter_t = "key not available!"
-#     twitter_ts = "key not available!"
+try:
+    twitter_ck = os.environ["TWITTER_CK"]
+    twitter_cs = os.environ["TWITTER_CS"]
+    twitter_t = os.environ["TWITTER_T"]
+    twitter_ts = os.environ["TWITTER_TS"]
+except KeyError:
+    twitter_ck = "key not available!"
+    twitter_cs = "key not available!"
+    twitter_t = "key not available!"
+    twitter_ts = "key not available!"
 
 def updateFuturecast():
     # Read in old Futurecast
@@ -60,26 +60,26 @@ def getFuturecast():
 
 
 def tweetFuturecast(Text,Link):
-    # twitter_auth_keys = {
-    #     "consumer_key"        : twitter_ck,
-    #     "consumer_secret"     : twitter_cs,
-    #     "access_token"        : twitter_t,
-    #     "access_token_secret" : twitter_ts
-    # }
+    twitter_auth_keys = {
+        "consumer_key"        : twitter_ck,
+        "consumer_secret"     : twitter_cs,
+        "access_token"        : twitter_t,
+        "access_token_secret" : twitter_ts
+    }
  
-    # auth = tweepy.OAuthHandler(
-    #         twitter_auth_keys['consumer_key'],
-    #         twitter_auth_keys['consumer_secret']
-    #         )
-    # auth.set_access_token(
-    #         twitter_auth_keys['access_token'],
-    #         twitter_auth_keys['access_token_secret']
-    #         )
-    # api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(
+            twitter_auth_keys['consumer_key'],
+            twitter_auth_keys['consumer_secret']
+            )
+    auth.set_access_token(
+            twitter_auth_keys['access_token'],
+            twitter_auth_keys['access_token_secret']
+            )
+    api = tweepy.API(auth)
     Text = Text.replace(')   to',') to')
     tweet = "🚨New #Gators Rivals Futurecast🚨\n\n"+Text+"\n\n🔗"+Link
-    print(tweet)
-    # status = api.update_status(status=tweet)
+    # print(tweet)
+    status = api.update_status(status=tweet)
 
 if __name__ == "__main__":
     updateFuturecast()
